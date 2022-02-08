7a0069106e216e93f11956a0a072789721126d66
Eliza <seeubanks12@gmail.com>
2022-02-08 17:40:26 -0500

    done

--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -3,7 +3,7 @@ const router = require("express").Router();
 const Movie = require("../models/Movie.model");
 const Celebrity = require("../models/Celebrity.model");
 
-
+//Create New Movie
 router.get("/movies/create", (req, res) => {
     Celebrity.find()
     .then((allCelebs) => {
@@ -30,9 +30,63 @@ router.post("/movies/create", (req, res) => {
     });
 });
 
+//Display all movies
 router.get("/movies", (req, res) => {
-    res.render("movies/movies");
+    Movie.find()
+    .then((allMovies) => {
+        console.log(allMovies);
+        res.render("movies/movies", {movies: allMovies});
+    })
+    .catch((err) => {
+        console.log(err);
+    });
 });
   
+//Display Movie Details
+router.get("/movies/:movieId", (req, res) => {
+    Movie.findById(req.params.movieId)
+    .populate("cast")
+    .then((foundMovie) => {
+        res.render("movies/movie-details", {foundMovie});
+    })
+    .catch((err) => {
+        console.log("Something went wrong");
+    });
+});
+
+//Delete Movies
+
+router.post("/movies/:movieId/delete", (req, res)=>{
+  Movie.findByIdAndRemove(req.params.movieId)
+  .then(()=>{
+    console.log("movie deleted")
+    res.redirect("/movies");
+  })
+  .catch(err=>{
+    console.log("Something went wrong:", err)
+  })
+})
+
+//Edit Movies
+
+router.get("/movies/:movieId/edit", (req, res) => {
+    Movie.findById(req.params.movieId).then((results) => {
+        Celebrity.find().then((celebrities) => {
+           res.render("movies/edit-movie", {celebrities, results});
+        });
+    });
+});
+
+router.post("/movies/:movieId/edit", (req, res) => {
+    Movie.findByIdAndUpdate(req.params.movieId, {
+       title: req.body.title,
+       genre: req.body.genre,
+       plot: req.body.plot,
+       cast: req.body.cast,
+    }).then((updateMovie) => {
+        console.log("Movie has been updated");
+        res.redirect(`/movies/${updateMovie.id}`);
+    });
+});
 
 module.exports = router;
